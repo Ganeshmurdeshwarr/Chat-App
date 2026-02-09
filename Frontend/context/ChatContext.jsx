fb2cf79e4c3fa809c84e02444a353dc3c1ad7165
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2026-02-09 10:56:33 +0530

    Initial MERN real-time chat application setup

---
+++ Frontend/context/ChatContext.jsx
@@ -0,0 +1,104 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [unseenMessages, setUnseenMessages] = useState({});
+
+  const { socket, axios } = useContext(AuthContext);
+
+  //Function to get all user for sidebar
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get("/api/messages/users");
+      if (data.success) {
+        setUsers(data.users);
+        setUnseenMessages(data.unseenMessages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+
+  // function to get messages for selected user
+
+  const getMessages = async (userId) => {
+    try {
+      const { data } = await axios.get(`/api/messages/${userId}`);
+      if (data.success) {
+        setMessages(data.messages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to send message to selected user
+
+  const sendMessage = async (messageData) => {
+    try {
+      const { data } = await axios.post(
+        `/api/messages/send/${selectedUser._id}`,
+        messageData,
+      );
+      if (data.success) {
+        setMessages((prev) => [...prev , data.newMessage]);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to subscribe to message for selected user
+  const subscribeToMessage = async () => {
+    if (!socket) return;
+
+    socket.on("newMessage", (newMessage) => {
+      if (selectedUser && newMessage.senderId === selectedUser._id) {
+        newMessage.seen = true;
+        setMessages((prev) => [...prev, newMessage]);
+        axios.put(`/api/messages/mark/${newMessage._id}`);
+      } else {
+        setUnseenMessages((prev) => ({
+          ...prev,
+          [newMessage.senderId]: prev[newMessage.senderId]
+            ? prev[newMessage.senderId] + 1
+            : 1,
+        }));
+      }
+    });
+  };
+
+  // Function to unsubscribe to message for selected user
+  const unsubscribeFromMessage = async () => {
+    if(socket) socket.off("newMessage")
+  }
+
+  useEffect(()=>{
+   subscribeToMessage();
+   return () => unsubscribeFromMessage()
+  },[socket , selectedUser])
+
+  const value = {
+    messages,
+    users,
+    selectedUser,
+    getMessages,
+    sendMessage,
+    getUser,
+    setSelectedUser,
+    unseenMessages,
+    setUnseenMessages,
+  };
+
+  return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
+};
