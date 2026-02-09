2b7d2ec3c8c1af445a25cb95179999ea7e54a0ac
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2026-02-09 14:45:45 +0530

    cors issue fixed

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // local dev
-      "https://your-frontend-url.vercel.app", // deployed frontend
+      "https://chat-app-git-main-ganesh-devadigas-projects.vercel.app/", // deployed frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
